f48883b063d1582d9fcfa70dc98f69cb8f7121d9
bhushanngage <bhushanngage@gmail.com>
2015-03-30 01:35:49 +0530

    added unused field to test findbugs

--- src/the/bhushan/tests/RoleTest.java
+++ src/the/bhushan/tests/RoleTest.java
@@ -1,5 +1,8 @@
 package the.bhushan.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -10,8 +13,19 @@ public class RoleTest {
 	@Test
 	public void testRoleStatus() {
 		Role role = new Role();
+		Role role1 = new Role();
 		role.setStatus("A");
 		Assert.assertEquals("Status of role doesn't match.", "A",
 				role.getStatus());
+	}
+	
+	@Test
+	public void testRoleCommands() {
+		Role role = new Role();
+		List<String> commands = new ArrayList<String>();
+		commands.add("the");
+		role.setCommands(commands);
+		Assert.assertEquals("inserted cmd is not matched to popped cmd.", "the", role.getCommands().get(0));
+		role.getCommands().retainAll(commands);
 	}
 }
